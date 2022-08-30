ebda9fece6a087438d8ec23c62fd224c92a82676
Ixidi <xlinux3@gmail.com>
2022-08-30 13:41:42 +0200

    missing cs files

---
+++ EloDopamine.Application/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EloDopamine.Application;
+
+internal static class Program {
+
+    public static void Main()
+    {
+        // var eloDopamine = new EloDopamine();
+        // eloDopamine.Start();
+    }
+    
+}
